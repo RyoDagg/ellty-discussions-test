0511427e1d6b904d4b95674b025fa82a26b495d1
RyoDagg <mannai.abdlkader@gmail.com>
2025-11-16 11:07:20 +0100

    Add @nestjs/config dependency and configure .env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,11 +4,15 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DiscussionsModule } from './discussions/discussions.module';
 import { MessagesModule } from './messages/messages.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     MongooseModule.forRoot(
-      process.env.MONGO_URI ?? 'mongodb://localhost:27017/ellty',
+      process.env.MONGODB_URI ?? 'mongodb://localhost:27017/ellty',
     ),
     DiscussionsModule,
     MessagesModule,
